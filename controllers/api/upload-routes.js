5214345d226ebb3f2832fb81c3b95bb8f6039d45
Zsvob <svobodazachary@gmail.com>
2022-03-01 11:29:25 -0500

    multer testing

---
+++ controllers/api/upload-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const path = require('path')
+
+const multer = require('multer');
+const { Post } = require('../../models');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Images')
+    },
+
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+
+
+
+const upload = multer({storage: storage})
+
+router.post('/upload', upload.single('petImage'), (req, res) => {
+    res.send("image Uploaded")
+    Post.create({
+              title: req.body.title,
+              post_url: req.body.post_url,
+              user_id: req.body.user_id
+            })
+});
+
+// router.post('/', upload.single('petImage'), (req, res) => {
+//     Post.create({
+//       title: req.body.title,
+//       post_url: req.body.post_url,
+//       user_id: req.body.user_id
+//     })
+//       .then(dbPostData => res.json(dbPostData))
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+//   });
+
+
+
+router.get('/', (req, res) => {
+  
+    res.render('upload');
+  });
+
+module.exports = router;
